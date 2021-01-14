258991eb865166f3267c84bcbc0fb661f2b9806d
Frida Persson <fridavbg@gmail.com>
2021-01-14 18:18:40 +0100

    trying to deploy this thing

---
+++ build/precache-manifest.e269d36df8c29c5df8252f705d0920f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6311a64e69daa9347fec09cb6a18f8b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec88c9be09d8d5f4d5e",
+    "url": "/static/css/main.7f2f6d29.chunk.css"
+  },
+  {
+    "revision": "0315f04d9b0b997d2429",
+    "url": "/static/js/2.69de1ae6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69de1ae6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec88c9be09d8d5f4d5e",
+    "url": "/static/js/main.1e4e1a46.chunk.js"
+  },
+  {
+    "revision": "dd03492f38894cfaf1ed",
+    "url": "/static/js/runtime-main.d5e0e7e4.js"
+  },
+  {
+    "revision": "97accc102d61269a7ac626385bf68698",
+    "url": "/static/media/backgroundImage1.97accc10.jpeg"
+  }
+]);
